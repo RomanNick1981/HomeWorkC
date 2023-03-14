037c34bb88311e9ae1d5ef3f9a741212f5e06d70
Roman <romaneskm@mail.ru>
2023-03-14 16:38:35 +0300

    Task 15 done

---
+++ task15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15
+Console.WriteLine("Введите день недели (введите число от 1 до 7)");
+int a = Convert.ToInt32(Console.ReadLine());
+string str;
+
+while (a < 1 || a > 7)
+{
+    a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введен неправильно день недели (введите число от 1 до 7)");
+}
+if (a > 5)
+    str = "Выходной день";
+else
+    str = "Будний день";
+Console.WriteLine(str);
