9ad63d9f7521c03f032fca17d715ea59a461f504
Roman <romaneskm@mail.ru>
2023-03-14 15:57:50 +0300

    Task10

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿//Task 10
+Console.Clear();
+Console.WriteLine("Введите трёзначное число: \n");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a > 999 || a < 100)
+{
+    Console.WriteLine("Ошибка, введите трёзначное число");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+a /= 10;
+a %= 10;
+Console.WriteLine($"Ответ {a}\n");
