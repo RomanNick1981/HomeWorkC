2733b57d031ade4549ec6d64619f9d2a706ac4aa
Roman <romaneskm@mail.ru>
2023-03-14 16:37:18 +0300

    Task 13 done

---
+++ Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task13
+Console.WriteLine("Введите число: ");
+
+string str;
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (a > 999)
+    a /= 10;
+
+if (a < 100)
+    str = "Нет третьей цифры";
+else 
+    str = $"Третья цифра: {a%10}";
+
+Console.WriteLine(str);
