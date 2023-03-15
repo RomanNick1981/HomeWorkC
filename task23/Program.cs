565ccc1d01b3058a8a8d9e82e22f9904b9cadce3
Roman <romaneskm@mail.ru>
2023-03-15 15:47:27 +0300

    Task23 Done

---
+++ task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Введите количество кубов:");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n < 1){
+    Console.WriteLine("Введите натуральное число!");
+    int n = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write(1);
+for(int i = 2; i <= n; i += 1)
+{
+    Console.Write(", " + Math.Pow(i, 3));
+}
+Console.WriteLine(".");
