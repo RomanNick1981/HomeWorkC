8444d009f79cfb9f881c6829f47e423e6b15dfe1
Roman <romaneskm@mail.ru>
2023-03-22 14:25:23 +0300

    Task27 Done

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿// Task27
+Console.Clear();
+Console.WriteLine("Введите число: ");
+String a = Console.ReadLine();
+int Sum = 0;
+for ( int i = 0; i < a.Length ;i++)
+{
+    Sum += a[i] - '0';
+}
+Console.WriteLine(Sum);
