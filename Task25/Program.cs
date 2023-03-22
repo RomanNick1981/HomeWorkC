d73f490323ff4b694f7e4d043ce58e044604a500
Roman <romaneskm@mail.ru>
2023-03-22 13:52:00 +0300

    Task25 done

---
+++ Task25/Program.cs
@@ -0,0 +1,9 @@
+﻿// Task25
+Console.Clear(); 
+Console.WriteLine("Введите два числа: "); 
+int A = int.Parse(Console.ReadLine()!); 
+int B = int.Parse(Console.ReadLine()!); 
+int result = 1; 
+for (int i = 1; i <= B; i++) 
+{ result = result * A; } 
+ Console.WriteLine(result);
