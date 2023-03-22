70229a08b66f24c5f23468d9f91c0fa1249a777a
Roman <romaneskm@mail.ru>
2023-03-22 14:52:47 +0300

    Task29 Done

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+void FillOutputArray(int[] array)
+{   Console.WriteLine("Сгенерированный массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= new Random().Next(-9,10);
+        if (i > 0)
+            Console.Write(", ");
+        Console.Write(array[i]);
+    }
+    Console.WriteLine();
+}
+Console.Clear();
+Console.WriteLine("Введите длину массива:");
+int n = int.Parse(Console.ReadLine());
+int[] a = new int[n];
+FillOutputArray(a);
