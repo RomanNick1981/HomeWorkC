ab5f31678a03e7d46a191464690d63e1347eccc3
Roman <romaneskm@mail.ru>
2023-03-25 15:05:12 +0300

    Task38 done

---
+++ task38/Program.cs
@@ -0,0 +1,35 @@
+﻿void InputArray(double[] array) // Генерация массива
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Math.Round(new Random().NextDouble() * 100 + 0, 2);
+}
+double Max(double[]array)
+{
+    double max = array[0];
+    for (int i = 0; i <array.Length; i++)
+    {
+        if (max<array[i])
+        {
+            max=array[i];
+        }
+    }
+    return max;
+}
+double Min(double[]array)
+{
+    double min = array[0];
+    for (int i = 0; i <array.Length; i++)
+    {
+        if (min>array[i])
+        {
+            min=array[i];
+        }
+    }
+    return min;
+}
+
+Console.Clear();
+double[] array = new double[5];
+InputArray(array);
+System.Console.WriteLine($"Массив: [{string.Join(" , " , array)}]");
+System.Console.WriteLine($"Max {Max(array)} - Min {Min(array)} = {Math.Round(Max(array)-Min(array), 2)}");
