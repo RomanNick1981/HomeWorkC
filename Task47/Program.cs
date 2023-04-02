aea16e0bf7d344b058ac8df8e479edaa2af1986a
Roman <romaneskm@mail.ru>
2023-04-02 15:16:27 +0300

    Task47 Done

---
+++ Task47/Program.cs
@@ -0,0 +1,24 @@
+﻿// Task 47
+void fill_matrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = Math.Round(new Random().NextDouble() * 20 - 10, 1);
+}
+
+void print_matrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j].ToString() + " ");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите размеры матрицы:");
+int[] mn = Console.ReadLine()!.Split(" ").Select(x => int.Parse(x)).ToArray();
+double[,] matr = new double[mn[0], mn[1]];
+fill_matrix(matr);
+print_matrix(matr);
