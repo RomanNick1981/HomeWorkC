b211b4ac85485225c3a32b6400b88b542071802d
Roman <romaneskm@mail.ru>
2023-04-06 19:26:16 +0300

    Task66

---
+++ task66/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int get_sum(int n, int m)
+{
+    int sum = 0;
+    // Пока разница между n и m не станет 0
+    while (n <= m)
+    {
+        sum += n;
+        n++;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine("Введите N");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(get_sum(n, m));
