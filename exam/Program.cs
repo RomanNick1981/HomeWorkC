cfcdb4fe3aa969cf030f448990702944a4a7b2d8
Roman <romaneskm@mail.ru>
2023-05-21 15:24:23 +0300

    Final_Version

--- exam/Program.cs
+++ exam/Program.cs
@@ -1,4 +1,5 @@
 ﻿// final task
+// Функция ввода пункта меню
 int menu(int error)
 {
     Console.Clear();
@@ -11,7 +12,7 @@ int menu(int error)
     int res = Convert.ToInt32(Console.ReadLine());
     return res;
 }
-
+// Функция вывода массива
 void print_array(int size, string[] arr)
 {
     Console.WriteLine("Полученный массив:");
@@ -25,39 +26,71 @@ void print_array(int size, string[] arr)
     }
     Console.WriteLine("]");
 }
-
-int res = menu(0);
+// Ввод пункта меню
+int res;
+try 
+{
+    res = menu(0);
+}
+catch (FormatException e)
+{
+    res = -1;
+}
 while ((res < 0) || (res > 2))
 {
-    res = menu(1);
+    try {
+        res = menu(1);
+    }
+    catch (FormatException e)
+    {
+        res = -1;
+    }
 }
+
 string[] array = new string[] {};
 int n = 0;
+// Обработка пункта меню
 switch (res)
 {
+    // Первый пункт - автозаполнение
     case 1:
         array = new string[] { "Hello", "2", "world", ":-)" };
         n = 4;
         break;
+    // Второй пункт - ввод с клавиатуры
     case 2:
-        Console.WriteLine("Введите размер массива");
-        n = Convert.ToInt32(Console.ReadLine());
+        while (n <= 0)
+        {
+            Console.WriteLine("Введите размер массива");
+            try {
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+            Console.WriteLine("Неверный размер массива!");
+                n = -1;
+            }
+        }
         array = new string[n];
         Console.WriteLine("Введите элементы массива слов, через новую строку");
         for(int i = 0; i < n; i++)
             array[i] = Console.ReadLine();
         break;
+    // В остальных случаях выход из окружения
     default:
         Environment.Exit(0);
         break;
 }
 
+// Создание размера нового массива
 int m = 0;
 for (int i = 0; i < n; i++)
 {
     if (array[i].Length <= 3)
         m++;
 }
+
+// Создание самого массива
 string[] result = new string[m];
 m = 0;
 for (int i = 0; i < n; i++)
